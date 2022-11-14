63d4c42e4de3c739f5a3ad247b20710eb0d165cb
Rémi Rubis <remi.rubis@yahoo.com>
2022-11-14 15:03:04 +0100

    chore(package): implement node-sass support to react

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
-import "./global.css";
+import "./global.scss";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { BrowserRouter as Router, Navigate, useRoutes } from "react-router-dom";
 
 import Header from "./components/sections/Header/Header";
