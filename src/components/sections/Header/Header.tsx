a77a2e12d3513f022caab33167c4dc209e5f509e
Rémi Rubis <remi.rubis@yahoo.com>
2022-11-14 20:59:49 +0100

    fix(deprecated): change deprecated content

--- src/components/sections/Header/Header.tsx
+++ src/components/sections/Header/Header.tsx
@@ -2,48 +2,46 @@ import "./Header.scss";
 
 import React from "react";
 import { Link } from "react-router-dom";
-import { useObserver } from "mobx-react";
+import { observer } from "mobx-react";
 
 import { useAuthStore } from "../../../providers/Auth.provider";
 
-const Header: React.FC = () =>
-  useObserver(() => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const { user, logout } = useAuthStore();
+const Header: React.FC = observer(() => {
+  const { user, logout } = useAuthStore();
 
-    return (
-      <header>
-        <h1>PostManager</h1>
-        <div className="header--nav">
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              {!user && (
-                <>
-                  <li>
-                    <Link to="/register">Register</Link>
-                  </li>
-                  <li>
-                    <Link to="/login">Login</Link>
-                  </li>
-                </>
-              )}
-              {user && (
-                <li className="logout" onClick={logout}>
-                  Logout
+  return (
+    <header>
+      <h1>PostManager</h1>
+      <div className="header--nav">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            {!user && (
+              <>
+                <li>
+                  <Link to="/register">Register</Link>
                 </li>
-              )}
-            </ul>
-          </nav>
-          {user && (
-            <div className="header--nav-user">
-              <p>{user.name}</p>
-            </div>
-          )}
-        </div>
-      </header>
-    );
-  });
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+              </>
+            )}
+            {user && (
+              <li className="logout" onClick={logout}>
+                Logout
+              </li>
+            )}
+          </ul>
+        </nav>
+        {user && (
+          <div className="header--nav-user">
+            <p>{user.name}</p>
+          </div>
+        )}
+      </div>
+    </header>
+  );
+});
 export default Header;
