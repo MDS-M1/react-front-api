3ac6cf3f4d0b94b1bdd5c21addf11add82b3ddec
Rémi Rubis <remi.rubis@yahoo.com>
2022-11-14 15:06:30 +0100

    feat(page/login): create login page

--- src/pages/Account/Login/Login.tsx
+++ src/pages/Account/Login/Login.tsx
@@ -1,10 +1,48 @@
-import "./Login.css";
+import "./Login.scss";
 
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import { login, LoginInput } from "../../../services/Auth.service";
 import { withRouter } from "../../../utils/withRouter";
 
-function Login() {
-  return <p>Login</p>;
-}
+const Login = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const registerPayload = {
+      email: formData.get("email"),
+      password: formData.get("password"),
+    };
+
+    login(registerPayload as LoginInput).then(() => navigate("/"));
+  };
+
+  return (
+    <>
+      <h2>Connect to your account</h2>
+      <p>View all posts with your personnal account</p>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="label--email">Email:</label>
+          <input name="email" type="email" id="label--email" required />
+        </div>
+        <div>
+          <label htmlFor="label--password">Password:</label>
+          <input
+            name="password"
+            type="password"
+            id="label--password"
+            required
+          />
+        </div>
+        <input type="submit" className="btn btn--primary" value="Submit" />
+      </form>
+    </>
+  );
+};
 
 export default withRouter(Login);
