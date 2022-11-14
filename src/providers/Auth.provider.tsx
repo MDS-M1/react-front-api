ddd4bc279493a562eebd9a1a471e80cd718a3ece
Rémi Rubis <remi.rubis@yahoo.com>
2022-11-15 00:47:22 +0100

    feat(ProtectedRoute): securize app whit auth verification on route navigation

--- src/providers/Auth.provider.tsx
+++ src/providers/Auth.provider.tsx
@@ -1,5 +1,6 @@
 import React, { createContext, useContext } from "react";
 import { useLocalObservable } from "mobx-react";
+import { Navigate, useLocation } from "react-router-dom";
 
 import { createAuthStore } from "../store/Auth.store";
 
@@ -18,4 +19,18 @@ export const AuthProvider = ({ children }: AuthProviderProps): JSX.Element => {
     <AuthContext.Provider value={authStore}>{children}</AuthContext.Provider>
   );
 };
+
 export const useAuthStore = () => useContext(AuthContext);
+
+export const ProtectedRoute = ({
+  children,
+}: AuthProviderProps): JSX.Element => {
+  const { user } = useAuthStore();
+  const location = useLocation();
+
+  if (!user) {
+    return <Navigate to={`/login?redirect=${location.pathname}`} replace />;
+  }
+
+  return children;
+};
