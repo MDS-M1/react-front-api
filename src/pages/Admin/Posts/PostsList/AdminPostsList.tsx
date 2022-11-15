d24811073dc563b722eeaf788539bb3884bb1d4e
Rémi Rubis <remi.rubis@yahoo.com>
2022-11-15 15:43:46 +0100

    feat(page/posts/adminpostslist): create static page to list all post as admin

---
+++ src/pages/Admin/Posts/PostsList/AdminPostsList.tsx
@@ -0,0 +1,15 @@
+import "./AdminPostsList.scss";
+
+import React from "react";
+import { withRouter } from "../../../../utils/withRouter";
+
+function AdminPostsList() {
+  return (
+    <>
+      <h2>All posts by admin</h2>
+      <p>List coming soon!</p>
+    </>
+  );
+}
+
+export default withRouter(AdminPostsList);
