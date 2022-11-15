efc0a220ff026866f0adf6765658036a61fc2e42
Rémi Rubis <remi.rubis@yahoo.com>
2022-11-15 15:50:23 +0100

    feat(page/user/list): create static page for listing all users registered

---
+++ src/pages/Admin/Users/UsersList/UsersList.tsx
@@ -0,0 +1,15 @@
+import "./UsersList.scss";
+
+import React from "react";
+import { withRouter } from "../../../../utils/withRouter";
+
+function UsersList() {
+  return (
+    <>
+      <h2>All users</h2>
+      <p>List of all users register coming soon!</p>
+    </>
+  );
+}
+
+export default withRouter(UsersList);
