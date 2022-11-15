0b8a5aa7528486d41136ef8bb421783d9dc6fd65
Rémi Rubis <remi.rubis@yahoo.com>
2022-11-15 15:48:58 +0100

    feat(page/user): create admin page to see user informations

---
+++ src/pages/Admin/Users/SingleUserPage/SingleUserPage.tsx
@@ -0,0 +1,15 @@
+import "./SingleUserPage.scss";
+
+import React from "react";
+import { withRouter } from "../../../../utils/withRouter";
+
+function SingleUserPage() {
+  return (
+    <>
+      <h2>Remi's profile</h2>
+      <p>User informations coming soon!</p>
+    </>
+  );
+}
+
+export default withRouter(SingleUserPage);
