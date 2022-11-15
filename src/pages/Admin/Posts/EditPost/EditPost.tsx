632bb7e5fd0eabcee5f3e1c22b7aab1a6e48a41f
Rémi Rubis <remi.rubis@yahoo.com>
2022-11-15 15:40:32 +0100

    feat(page/posts/editpost): create static page to edit a post

---
+++ src/pages/Admin/Posts/EditPost/EditPost.tsx
@@ -0,0 +1,15 @@
+import "./EditPost.scss";
+
+import React from "react";
+import { withRouter } from "../../../../utils/withRouter";
+
+function EditPost() {
+  return (
+    <>
+      <h2>Edit a post</h2>
+      <p>Form coming soon!</p>
+    </>
+  );
+}
+
+export default withRouter(EditPost);
