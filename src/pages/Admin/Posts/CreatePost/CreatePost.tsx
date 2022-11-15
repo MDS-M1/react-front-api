13ba7ca6df832fe30ad1f48c8373abf382ae0817
Rémi Rubis <remi.rubis@yahoo.com>
2022-11-15 15:39:36 +0100

    feat(page/posts/createpost): create static page to create new post

---
+++ src/pages/Admin/Posts/CreatePost/CreatePost.tsx
@@ -0,0 +1,15 @@
+import "./CreatePost.scss";
+
+import React from "react";
+import { withRouter } from "../../../../utils/withRouter";
+
+function CreatePost() {
+  return (
+    <>
+      <h2>Create a new post</h2>
+      <p>Form coming soon!</p>
+    </>
+  );
+}
+
+export default withRouter(CreatePost);
